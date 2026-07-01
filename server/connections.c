eaced3e62f05059340f0338229e21ff99cc642f4
Guru-codes07 <2020gurup@gmail.com>
2026-07-02 02:19:49 +0530

    completed connections.h ( header file )

---
+++ server/connections.c
@@ -0,0 +1,5 @@
+#include "common.h"
+#include "connections.h"
+
+// list of all connected clients
+client_t *client[MAX_CLIENTS] = {0};
--- server/connections.h
+++ server/connections.h
@@ -1,8 +1,29 @@
 #ifndef CONNECTIONS_H
 #define CONNECTIONS_H
+#include "common.h"
 
+typedef struct     // client struct  
+{
+    int socket_fd;
+    char name[NAME_SIZE];
+}client_t;
 
+// functions which are used to manage clients
+// add a new client to the clients array
+int add_client(client_t *client);
 
+// remove a client from the clients array
+void remove_client(client_t *client);
+
+/* To broadcast the message to everyone except
+the one who sended it (sender) */
+void broadcast_message(const char *message,client_t *sender);
+
+// function that shows online users
+void send_online_list(client_t *client);
+
+/* function that sends private message to a single user */
+int send_private_message(client_t *sender,const char *target_name,const char *text);
 
 
 
