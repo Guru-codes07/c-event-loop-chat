8516e4680d3427b60e1277db6850f8267c813c5f
Guru-codes07 <2020gurup@gmail.com>
2026-07-03 10:45:10 +0530

    create commands.h and command.c

---
+++ server/commands.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"commands.h"
+#include"connections.h"
+#include"common.h"
---
+++ server/commands.h
@@ -0,0 +1,10 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+#include"connections.h"
+#include"common.h"
+
+/* Function for declaring /who and /w commands in the program
+which will be used by the clients */
+void handle_commands(client_t *client,const char *command);
+
+#endif
