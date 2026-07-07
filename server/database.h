ae5d9e345bb78737a54b9be8d3ae3c978527d66f
Guru-codes07 <2020gurup@gmail.com>
2026-07-07 12:52:15 +0530

    completed database.h

--- server/database.h
+++ server/database.h
@@ -40,12 +40,22 @@ int db_store_mesage(const char *sender_name,const char *message_text,time_t time
 /* store a private chat message */
 int db_store_private_message(const char *sender_name,const char *recipient_name,const char *message_text,time_t timestamp,uint32_t messsasge_id);
 
+/* retrieve a public message */
+StoredMessage *db_get_recent_messsages(int limit,int *out_count);
 
+/* retrieve chat history for a specific date */
+StoredMessage *db_get_messages_by_date(int year , int month ,int day, int *out_count);
 
+/* retrieve chat history of a private chat between two users */
+StoredMessage *db_get_private_history(const char *user1,const char *user2, int *out_count);
 
+/* to delete old messages after a particular number of days */
+int db_cleanup_old_messages(int days_old);
 
+/* to get database statistics */
+int db_get_stats(int64_t *out_msg_count , int64_t *out_pm_count);
 
-
-
+// to close the database we use cleanup() func
+int db_cleanup(void);
 
 #endif
